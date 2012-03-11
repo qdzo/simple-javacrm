ff09d3357392a596d6f5378414e7f67aaf366b69
banch.idea19 <banch.idea19@ebdc6534-c0f3-569c-c24e-863fe95f7431>
2012-03-11 21:47:42 +0000

    DB classes  which be required for DAO pattern. first upload.

---
+++ branches/model/Destribution.java
@@ -0,0 +1,24 @@
+package model;
+
+public class Destribution {
+
+	int id;
+	String DateTime;
+	int ManagerId;
+	String ManagerFirstName;
+	String ManagerSecondName;
+	int ClientId;
+	String ClientFirstName;
+	String ClientSecondName;
+	int ProductId;
+	String ProductName;
+	
+	
+	
+	
+	
+	
+	
+	
+	
+}
