887f4f7a6763b33d85f5fb6900b449276f69a610
banch.idea19 <banch.idea19@ebdc6534-c0f3-569c-c24e-863fe95f7431>
2012-03-27 12:36:28 +0000

    implements a business-model methods, such as getter and setter,validate, validateDecimal.

--- branches/model/Product.java
+++ branches/model/Product.java
@@ -2,4 +2,97 @@ package model;
 
 public class Product {
 
+	private final Integer productId;
+	private String nameProduct;
+	private long price;
+	private int summary;
+	private String description;
+	
+	Product(Integer id){
+		this.productId = id;
+	}
+
+	public String getNameProduct() {
+		return nameProduct;
+	}
+
+	public void setNameProduct(String nameProduct) {
+		if(validate(nameProduct))
+		this.nameProduct = nameProduct;
+	}
+
+	public long getPrice() {
+		return price;
+	}
+
+	public void setPrice(long price) {
+		if(validateDecimalLong(price))
+		this.price = price;
+	}
+
+	
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String descriprion) {
+		if(validate(description))
+		this.description = descriprion;
+	}
+	
+
+	
+	public Integer getProductId() {
+		return productId;
+	}
+
+	
+	public int getSummary() {
+		return summary;
+	}
+
+	
+	public void setSummary(int summary) {
+		if(validateDecimal(summary))
+		this.summary = summary;
+	}
+	
+	
+	public void provideSell(){
+		this.summary=-1;
+	}
+	
+	
+	public void provideSell(int summToSell){
+		if(validateDecimal(summToSell))
+		this.summary = summToSell;
+	}
+	
+	
+	protected boolean validate(String line){
+		if(line.length()==0||line==null){
+			System.out.println("Required an argument!");
+			return false;
+		}
+		return true;
+	}
+	
+	protected boolean validateDecimal(int dec){
+		if(dec==-1||dec==0){
+			System.out.println("Required a right decimal argument!");
+			return false;
+		}
+		return true;
+	}
+	
+	private boolean validateDecimalLong(long decLong) {
+		if(decLong==-1||decLong==0){
+			System.out.println("Required a right long decimal argument!");
+			return false;
+		}
+		return true;
+	}
+
+	
 }
