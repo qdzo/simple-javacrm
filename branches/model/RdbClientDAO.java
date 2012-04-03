09ccae51fe7336f8160ae61bba54f1d8aa74da89
banch.idea19 <banch.idea19@ebdc6534-c0f3-569c-c24e-863fe95f7431>
2012-04-03 05:55:29 +0000

    start to implement the DAO methods

--- branches/model/RdbClientDAO.java
+++ branches/model/RdbClientDAO.java
@@ -1,45 +1,178 @@
 package model;
 
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.Collection;
 
 import javax.sql.RowSet;
 
 public class RdbClientDAO implements ClientDAO {
 	
+	private final static String INSERT = "insert into person(first_name,second_name,tel,email,belong,priority) values (";
+	private final static String SEMICOLON = ");";
+	private final static String SELECT = "select * from person where ";
+	private final static String UPDATE = "update person set ";
+	private final static String DELETE = "delete from person where pers_id = ";
+	private final static String QUOTES = "\'";
+	private final static String QUOTESWITHKOMA = "\',\'";
+	private final static String GROUP = "2";
 //==================================================================================================	
 	
 	public int insertClient(Client client){
 	
-		return 0;
+		int result = 0;
+		try {
+			JDCConnection connection = RdbDAOFactory.createConnection();
+			Statement statement = connection.createStatement();
+			String sql = INSERT+QUOTES+client.getFirstName()+QUOTESWITHKOMA+client.getSecondName()+
+					QUOTESWITHKOMA+client.getTelephone()+QUOTESWITHKOMA+client.getEmail()+QUOTESWITHKOMA+
+					GROUP+QUOTESWITHKOMA+client.getPriority()+QUOTES+SEMICOLON;
+			System.out.println(sql);
+			result = statement.executeUpdate(sql);
+		} catch (ClassNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (InstantiationException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IllegalAccessException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		
+		return result;
 	}
 
 	@Override
 	public boolean deleteClient(Client client) {
+		int result=0;
 		// TODO Auto-generated method stub
+		try {
+			JDCConnection connection = RdbDAOFactory.createConnection();
+			Statement statement = connection.createStatement();
+			result = statement.executeUpdate(null);
+		} catch (ClassNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (InstantiationException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IllegalAccessException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		if(result==1)
+			return true;
+		else
 		return false;
 	}
 
 	@Override
 	public Client findClient(Client client) {
 		// TODO Auto-generated method stub
+		try {
+			JDCConnection connection = RdbDAOFactory.createConnection();
+			Statement statement = connection.createStatement();
+			ResultSet resultSet = statement.executeQuery(null);
+			} catch (ClassNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (InstantiationException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IllegalAccessException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
 		return null;
 	}
 
 	@Override
 	public boolean updateClient(Client client) {
 		// TODO Auto-generated method stub
+		int result = 0;
+		try {
+			JDCConnection connection = RdbDAOFactory.createConnection();
+			Statement statement = connection.createStatement();
+			result = statement.executeUpdate("select");
+		} catch (ClassNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (InstantiationException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IllegalAccessException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		if(result==1)
+			return true;
+		else
 		return false;
 	}
 
 	@Override
 	public RowSet selectClientsRS(Client client) {
 		// TODO Auto-generated method stub
+		try {
+			JDCConnection connection = RdbDAOFactory.createConnection();
+			Statement statement = connection.createStatement();
+			ResultSet resultSet = statement.executeQuery(null);
+		} catch (ClassNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (InstantiationException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IllegalAccessException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
 		return null;
 	}
 
 	@Override
 	public Collection<Client> selectClientsTO(Client client) {
 		// TODO Auto-generated method stub
+		try {
+			JDCConnection connection = RdbDAOFactory.createConnection();
+			Statement statement = connection.createStatement();
+			ResultSet resultSet = statement.executeQuery(null);
+		} catch (ClassNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (InstantiationException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IllegalAccessException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
 		return null;
 	}
 
