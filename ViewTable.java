776c5be70c806cacb2f5059b1433c3b1f7ed61b7
banch.idea19 <banch.idea19@ebdc6534-c0f3-569c-c24e-863fe95f7431>
2012-04-10 22:41:03 +0000

    fixed ViewTable presentation of colums from different models, and no real change in Heap

--- ViewTable.java
+++ ViewTable.java
@@ -6,19 +6,17 @@ import javax.swing.table.TableModel;
 
 public class ViewTable extends JTable implements IObserver {
 
-	
 	/**
 	 * 
 	 */
 	private static final long serialVersionUID = -974433386690900079L;
 
-
+	
 
 	@Override
 	public void notifyEvent(TableModel tableEvent) {
 		this.setModel(tableEvent);
-		this.revalidate();
-		this.repaint();
+		this.createDefaultColumnsFromModel();
     }
 	
 	
