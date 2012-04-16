a25dee809a6e09963a5946faa672964a6f181a20
banch.idea19 <banch.idea19@ebdc6534-c0f3-569c-c24e-863fe95f7431>
2012-04-16 05:22:57 +0000

    added buildQuery method

--- branches/model/RdbManagerDAO.java
+++ branches/model/RdbManagerDAO.java
@@ -3,20 +3,26 @@ package model;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.ArrayList;
 import java.util.Collection;
+import java.util.List;
 
 import javax.sql.RowSet;
 
 public class RdbManagerDAO implements ManagerDAO{
 
-	private final static String INSERT = "insert into person(first_name,second_name,tel,email,belong,priority) values (";
-	private final static String SEMICOLON = ");";
-	private final static String SELECT = "select * from person where ";
-	private final static String UPDATE = "update person set ";
-	private final static String DELETE = "delete from person where pers_id = ";
-	private final static String QUOTES = "\'";
-	private final static String QUOTES_WITH_COMMA = "\',\'";
-	private final static String WHERE = "where ";
+	private final static String tableName = "person";
+	private final static String PERS_ID = "pers_id";
+	private final static String FIRST_NAME = "first_name";
+	private final static String SECOND_NAME = "second_name";
+	private final static String TELEPHONE = "tel";
+	private final static String EMAIL = "email";
+	private final static String GROUP_ID = "group_id";
+	private final static String STATUS_ID = "status_id";
+	public static final int SELECT = 1;
+	public static final int INSERT = 2;
+	public static final int UPDATE = 3;
+	public static final int DELETE = 4;
 	
 	@Override
 	public int insertManager(Manager manager) {
@@ -201,6 +207,49 @@ public class RdbManagerDAO implements ManagerDAO{
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
+		return null;
+	}
+	
+	public static String buildQuery(Manager manager,int queryType){
+		
+		Integer id = manager.getId();
+		List<String> columns = new ArrayList<String>();
+		List<String> values = new ArrayList<String>();
+		if(manager.getFirstName()!=null){
+			columns.add(FIRST_NAME);
+			values.add(manager.getFirstName());
+		}
+		if(manager.getSecondName()!=null){
+			columns.add(SECOND_NAME);
+			values.add(manager.getSecondName());
+		}
+		if(manager.getTelephone()!=null){
+			columns.add(TELEPHONE);
+			values.add(manager.getTelephone());
+		}
+		if(manager.getEmail()!=null){
+			columns.add(EMAIL);
+			values.add(manager.getEmail());
+		}
+		if(manager.getStatus()!=null){
+			columns.add(STATUS_ID);
+			Integer statusId = manager.getStatus().getValue();
+			values.add(statusId.toString());
+		}
+		Object obj = columns.toArray();
+		String[] col =  (String[]) obj;
+		String [] val = (String[]) values.toArray();
+		switch(queryType){
+		case INSERT:
+			return RdbDAOFactory.sql.buildInsert(tableName,col,val);
+		case SELECT:
+			return RdbDAOFactory.sql.buildSelect(tableName,col,val);
+		case UPDATE:
+			return RdbDAOFactory.sql.buildUpdate(tableName,col,val, PERS_ID, id);
+		case DELETE:
+			return RdbDAOFactory.sql.buildDelete(tableName, PERS_ID, id, STATUS_ID);
+		}
+		
 		return null;
 	}
 
