68d0925bfd1baf1f260ec4dd6da97599c16957cc
banch.idea19 <banch.idea19@ebdc6534-c0f3-569c-c24e-863fe95f7431>
2012-04-16 05:25:55 +0000

    added new class

---
+++ SQLBuilder.java
@@ -0,0 +1,77 @@
+package model;
+
+public class SQLBuilder {
+	
+	
+	
+	
+	public String buildDelete(String tableName,String columnIdName,int id,String statusColumn) {
+		String sql = "UPDATE "+tableName+" SET "+statusColumn+"=2 WRERE "+columnIdName+"="+id+";";
+		return sql;
+	}
+
+	
+	public String buildUpdate(String tableName,String[] columns, String[] values,String columnId,int id) {
+		String sql = "UPDATE "+tableName+" SET ";
+			for(int i=0;i<columns.length;i++){
+				sql+= columns[i]+ "= \""+values[i]+"\"";
+				if(i+1<columns.length)
+					sql+= " AND ";
+		 }
+		 sql +=" WHERE "+columnId+"="+id+";";
+		return sql;
+	}
+
+	
+	public String buildInsert(String tableName, String[] columns,String[] values) {
+		String sql = "INSERT INTO "+tableName+"(";
+		for(int i=0;i<columns.length;i++){
+			sql+=columns[i];
+			if(i+1<columns.length)
+				sql+=",";
+		}
+		sql+= ") VALUES (\'";
+		for(int i=0;i<columns.length;i++){
+			sql+= values[i]+"\'";
+			if(i+1<columns.length)
+				sql+=",\'";
+		}
+		sql+=");";
+		return sql;
+	}
+
+	
+	public String buildSelect(String tableName,String[] columns,String[] values){
+		String sql = "SELECT * FROM "+tableName+" WHERE ";
+		for(int i=0;i<columns.length;i++){
+			sql+= columns[i]+ "= \""+values[i]+"\"";
+			if(i+1<columns.length)
+				sql+= " AND ";
+	 }
+	 sql +=";";
+	return sql;
+	}
+	
+	public String buildSmartSelect(String[] selectedColumns,String[] tableNames,String[] equivalentColumns,String[] columns,String[] values){
+		String sql = "SELECT ";
+		for(int i=0;i<selectedColumns.length;i++){
+			sql+=selectedColumns[i];
+			if(i+1<selectedColumns.length)
+				sql+=",";
+		}
+		sql+=" FROM "+tableNames[0];
+		for(int i=0;i<tableNames.length;i++){
+			sql+= " LEFT OUTER JOIN "+tableNames[i+1]+" ON "+ equivalentColumns[i];
+		}
+		if(columns.length>0)
+			sql+=" WHERE ";
+		for(int i=0;i<columns.length;i++){
+			sql+=columns[i]+"="+values[i];
+			if(i+1<columns.length)
+				sql+= " AND ";
+		}
+		sql+=";";
+		return sql;
+	}
+
+}
